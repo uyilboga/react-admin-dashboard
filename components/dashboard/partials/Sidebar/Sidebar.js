9ce848593db5f684a51c13719cd5e6bbbcf994a6
uyilboga <ylbogaufuk@gmail.com>
2022-06-25 19:32:09 +0300

    sidebar update

--- components/dashboard/partials/Sidebar/Sidebar.js
+++ components/dashboard/partials/Sidebar/Sidebar.js
@@ -1,14 +1,43 @@
 import Link from "next/link";
+import {Button} from "react-bootstrap";
 
 function Sidebar() {
 	return (
 		<div className="dashboard-sidebar">
-			<div className="sidebar-log">
+			<div className="sidebar-logo">
 				<Link href={'/dashboard'}>
 					<a>
 						Admin Dashboard
 					</a>
 				</Link>
+			</div>
+			<div className="sidebar-menu">
+				<ul>
+					<li>
+						<Link href={'/dashboard'}>
+							<a>Dashboard</a>
+						</Link>
+					</li>
+					<li>
+						<Link href={'/dashboard'}>
+							<a>Services</a>
+						</Link>
+					</li>
+					<li>
+						<Link href={'/dashboard'}>
+							<a>Users</a>
+						</Link>
+					</li>
+
+					<li>
+						<Link href={'/dashboard'}>
+							<a>Settings</a>
+						</Link>
+					</li>
+				</ul>
+			</div>
+			<div className="sidebar-footer">
+				<Button variant="primary" href={'/'}>Need Help?</Button>
 			</div>
 		</div>
 	)
