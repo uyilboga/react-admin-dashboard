715f0d5a175f00125317a1983a2c97142a2c6a11
uyilboga <ylbogaufuk@gmail.com>
2022-06-27 01:38:18 +0300

    update

--- components/dashboard/partials/Sidebar/Sidebar.js
+++ components/dashboard/partials/Sidebar/Sidebar.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import {Button} from "react-bootstrap";
+import {User, Home, Grid, Settings} from "react-feather";
 
 function Sidebar() {
 	return (
@@ -7,37 +8,40 @@ function Sidebar() {
 			<div className="sidebar-logo">
 				<Link href={'/dashboard'}>
 					<a>
-						Admin Dashboard
+						<img src={'/main/logo.svg'} alt=""/>
 					</a>
 				</Link>
 			</div>
 			<div className="sidebar-menu">
 				<ul>
+					<li><span className="menu-title">DASHBOARD</span></li>
 					<li>
 						<Link href={'/dashboard'}>
-							<a>Dashboard</a>
+							<a className="active"><span className="icon"><Home size={18}/></span> Dashboard</a>
 						</Link>
 					</li>
+					<li><span className="menu-title">TRANSACTİONS</span></li>
 					<li>
 						<Link href={'/dashboard'}>
-							<a>Services</a>
+							<a><span className="icon"><Grid size={18}/></span> Services</a>
 						</Link>
 					</li>
+					<li><span className="menu-title">USER MANAGEMENT</span></li>
 					<li>
 						<Link href={'/dashboard'}>
-							<a>Users</a>
+							<a><span className="icon"><User size={18}/></span> Users</a>
 						</Link>
 					</li>
 
 					<li>
 						<Link href={'/dashboard'}>
-							<a>Settings</a>
+							<a><span className="icon"><Settings size={18}/></span> Settings</a>
 						</Link>
 					</li>
 				</ul>
 			</div>
 			<div className="sidebar-footer">
-				<Button variant="primary" href={'/'}>Need Help?</Button>
+				<Button variant="secondary" href={'/'}>Need Help?</Button>
 			</div>
 		</div>
 	)
--- components/dashboard/partials/Topbar/Topbar.js
+++ components/dashboard/partials/Topbar/Topbar.js
@@ -1,8 +1,39 @@
+import {Form, FormGroup, FormControl} from 'react-bootstrap'
+import {Search, Bell, Settings, Sun, Moon} from "react-feather";
+import {useState} from "react";
+
 function Topbar() {
+
+	const [darkMode, setDarkMode] = useState(false);
+
+	const searchSubmit = () => {
+
+	}
+
 	return (
 		<>
 			<div className="topbar">
-				Topbar
+				<div className="topbar-wrapper">
+					<div className="search">
+						<form onSubmit={() => searchSubmit}>
+							<div className="form-item">
+								<FormControl className="Control" placeholder="Search Something..."/>
+								<span className="icon">
+									<Search size={21}/>
+								</span>
+							</div>
+						</form>
+					</div>
+					<div className="topbar-right">
+						<div className="topbar-options">
+							<ul>
+								<li><a href="#" className="notifications"><Bell size={21}/></a></li>
+								<li><a href="#" className="settings"><Settings size={21}/></a></li>
+								<li><a href="#" className="darkmode">{ darkMode ? <Sun size={21}/> : <Moon size={21}/> }</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
 			</div>
 		</>
 	)
